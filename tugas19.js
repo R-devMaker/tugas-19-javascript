68e29d514d89633febda6315b0e6266ec045e110
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:28:31 +0700

    Tugas 19 JavaScriptPengumpulan tugas 19 JavaScript Niomic

---
+++ tugas19.js
@@ -0,0 +1,18 @@
+function repeatBag1(){
+    var isi = "Semangat...";
+    console.log(isi.repeat(5));
+}
+
+repeatBag1();
+
+console.log();
+var bintang = "*";
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(25));
+console.log(bintang.repeat(45));
+console.log(bintang.repeat(65));
+console.log(bintang.repeat(95));
+console.log(bintang.repeat(65));
+console.log(bintang.repeat(45));
+console.log(bintang.repeat(25));
+console.log(bintang.repeat(5));
